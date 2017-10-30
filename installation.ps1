39560a724c8faecefdbb580149a85d2ad7ed3e94
gavstechteam <33223073+gavstechteam@users.noreply.github.com>
2017-10-30 22:32:32 +0530

    Update installation.ps1

--- installation.ps1
+++ installation.ps1
@@ -1,3 +1,54 @@
+<# Altering the time zone of the machine #>
+cd $env:WINDIR\system32\
+tzutil /s 'India Standard Time'
+
+<# Install Chocolatey  #>
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+
+<# VC++ runtimes needed as dependencies for other installations #>
+choco install vcredist-all -y
+
+<# SQL Server Management Studio #>
+choco install sql-server-management-studio -y
+		
+<# Install IIS Server #>
+Install-WindowsFeature -Name web-server -IncludeManagementTools
+Install-WindowsFeature -Name Web-Asp-Net45
+
+<# Configure the IIS server #>
+Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName Port -Value 8080
+netsh advfirewall firewall add rule name="HTTP Web Application" dir=in action=allow protocol=TCP localport=8080
+
+
+#If($InstallIIS -eq 'true')
+#{
+    <# Install the IIS server #>
+    Install-WindowsFeature -Name web-server -IncludeManagementTools
+    Install-WindowsFeature -Name Web-Asp-Net45
+
+    <# Configure the Firewall #>
+    #Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName Port -Value $WebServerPort
+    #netsh advfirewall firewall add rule name="HTTP Web Application" dir=in action=allow protocol=TCP localport=$WebServerPort
+
+    If($WebServerPackage -ne '')
+    {
+        # Folders
+#        New-Item -ItemType Directory c:\temp
+#        New-Item -ItemType Directory c:\sites
+
+        # Download app package
+#        Invoke-WebRequest  $WebServerPackage -OutFile c:\temp\app.zip
+#        Expand-Archive C:\temp\app.zip c:\sites
+
+        # Configure iis
+#        Remove-WebSite -Name "Default Web Site"
+        #Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
+#        New-Website -Name "Application" -Port 80 -PhysicalPath C:\sites\ -ApplicationPool DefaultAppPool & iisreset
+    }
+#}
+
+
 <# Import environment variables #>
  $InstallIIS = $env:installiis
  $InstallTomcat = $env:installtomcat
@@ -34,3 +85,6 @@ If($InstallMySQL -eq 'true')
 	<# MySQL #>
 	choco install mysql -y
 }
+
+
+
