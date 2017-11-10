397856fb9c4f10e4329bc9a6163a1cfc92fb9241
gavstechteam <33223073+gavstechteam@users.noreply.github.com>
2017-11-10 18:47:20 +0530

    Update sqldatamigration.ps1

--- sqldatamigration.ps1
+++ sqldatamigration.ps1
@@ -62,6 +62,42 @@ Function ConnectionString([string] $ServerName, [string] $DbName, [string] $User
 Write-Host "Started" 
 $SrcConnStr = ConnectionString $SrcServer $SrcDatabase $SrcUserName $SrcPwd
 $SrcConn  = New-Object System.Data.SqlClient.SQLConnection($SrcConnStr)
+
+Write-Host "Create Source Table"
+$cmd  = new-object System.Data.SQLClient.SQLCommand
+$cmd.CommandText = "CREATE TABLE [dbo].[SourceDB1](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](100) NULL,
+	[LastName] [nvarchar](100) NULL,
+	[DOB] [date] NULL,
+	[Email] [nvarchar](100) NULL,
+	[Department] [nvarchar](100) NULL,
+	[SSIN] [nvarchar](100) NULL,
+	[AccountNumber] [nvarchar](100) NULL,
+	[Address] [nvarchar](400) NULL,
+	[DrivingLicence] [nvarchar](400) NULL
+) ON [PRIMARY]"
+$SrcConn.Open()
+$cmd.Connection = $SrcConn
+$cmd.ExecuteNonQuery()
+
+$cmd.CommandText = "declare @sqlstmt nvarchar(1000)
+set @sqlstmt ='begin
+declare @iCnt int = 1
+declare @iCntStr varchar(max)
+	while (@iCnt <= 1000)
+		begin
+		set @iCntStr = cast( @iCnt as varchar(3))
+		insert into SourceDB1 ([FirstName],[LastName],[DOB],[Email],[Department],[SSIN],[AccountNumber],[Address], [DrivingLicence] ) values (''FirstName'' + @iCntStr, ''LastName'' + @iCntStr , dateadd(MONTH, convert(int,100*rand()), ''1-Jan-1980'') , ''FirstName'' + @iCntStr + ''@gavstech.com'', ''Department'' + @iCntStr , ''100000000'' + @iCnt, ''1250000'' + @iCnt, ''Address, No: '' + @iCntStr , ''Drive000'' + @iCntStr )
+		set  @iCnt = @iCnt +1
+		end
+end'
+
+exec sp_executesql @sqlstmt"
+$cmd.Connection = $SrcConn
+$cmd.ExecuteNonQuery()
+$SrcConn.Close()
+
 $CmdText = "SELECT * FROM " + $SrcTable
 $SqlCommand = New-Object system.Data.SqlClient.SqlCommand($CmdText, $SrcConn) 
 $SrcConn.Open()
@@ -72,16 +108,29 @@ Try
     $DestConnStr = ConnectionString $DestServer $DestDatabase $DestUserName $DestPwd
     Write-Host "DEST connected"
 
+    write-host "Create Target Table"
+$cmd1  = new-object System.Data.SQLClient.SQLCommand
+$cmd1.CommandText = "CREATE TABLE [dbo].[TargetDB](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](100) NULL,
+	[LastName] [nvarchar](100) NULL,
+	[Email] [nvarchar](100) NULL,
+	[Department] [nvarchar](100) NULL
+) ON [PRIMARY]"
+$tgtConn  = New-Object System.Data.SqlClient.SQLConnection($DestConnStr)
+$tgtConn.Open()
+$cmd1.Connection = $tgtConn
+$cmd1.ExecuteNonQuery()
+
     $bulkCopy = New-Object Data.SqlClient.SqlBulkCopy($DestConnStr, [System.Data.SqlClient.SqlBulkCopyOptions]::KeepIdentity)
     $bulkCopy.DestinationTableName = $DestTable
     
-    $bulkcopy.columnmappings.add("id", "id");
-    $bulkcopy.columnmappings.add("firstname", "firstname");
-    $bulkcopy.columnmappings.add("lastname", "lastname");
-    $bulkcopy.columnmappings.add("email", "email");
-    $bulkcopy.columnmappings.add("department", "department");
-    $bulkcopy.columnmappings.add("address", "address");
-    $bulkcopy.batchsize = 10
+    $bulkcopy.columnmappings.add("Id", "Id");
+    $bulkcopy.columnmappings.add("FirstName", "FirstName");
+    $bulkcopy.columnmappings.add("LastName", "LastName");
+    $bulkcopy.columnmappings.add("Email", "Email");
+    $bulkcopy.columnmappings.add("Department", "Department");
+    $bulkcopy.batchsize = 100
     $bulkcopy.bulkcopytimeout = 120
 
     Write-Host "Copying Started"
