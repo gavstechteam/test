e3c53420b6aae624a02f8dd965e6746aea6b17ac
gavstechteam <33223073+gavstechteam@users.noreply.github.com>
2017-11-12 21:23:16 +0530

    Update sqldatamigration.ps1

--- sqldatamigration.ps1
+++ sqldatamigration.ps1
@@ -5,33 +5,33 @@ Param
       [string] $SrcServer = "(local)",
 
       [parameter(Mandatory = $false)]
-      [string] $SrcDatabase = "zirrus",
+      [string] $SrcDatabase = "sourceDB",
 
       [parameter(Mandatory = $false)]
       [string] $SrcTable = "sourcedb",
 
       [parameter(Mandatory = $false)]
-      [string] $SrcUserName = "zirrusdev",
+      [string] $SrcUserName = "srcuser",
 
       [parameter(Mandatory = $false)]
-      [string] $SrcPwd = "Z!rrus99",
+      [string] $SrcPwd = "Zirrus@123",
 
       [parameter(Mandatory = $false)]
       [string] $DestServer = "(local)",
 
       # Name of the destination database is optional. When omitted, it is set to the source database name.
       [parameter(Mandatory = $false)]
-      [string] $DestDatabase = "zirrustest", 
+      [string] $DestDatabase = "DestDB", 
 
       # Name of the destination table is optional. When omitted, it is set to the source table name.
       [parameter(Mandatory = $false)]
       [string] $DestTable = "targetdb", 
 
       [parameter(Mandatory = $false)]
-      [string] $DestUserName = "zirrustest",
+      [string] $DestUserName = "destuser",
 
       [parameter(Mandatory = $false)]
-      [string] $DestPwd = "Z!rrus99",
+      [string] $DestPwd = "Zirrus@123",
 
       [switch] $Truncate = $false # Include this switch to truncate the destination table before the copy.
   )
@@ -47,61 +47,141 @@ Param
     $DestTable = $SrcTable
  }
 
- 
- If ($Truncate) 
- {
-    $TruncateSql = "TRUNCATE TABLE " + $DestTable
-    Sqlcmd -S $DestServer -d $DestDatabase -Q $TruncateSql
- }
 
 Function ConnectionString([string] $ServerName, [string] $DbName, [string] $UserName, [string] $pwd)
 {
     "Data Source=$ServerName;Initial Catalog=$DbName;User Id=$UserName;Password=$pwd;Integrated Security=False;"
 }
 
-Write-Host "Started" 
+Function ReOpenConnection([System.Data.SqlClient.SqlConnection] $conn)
+{
+    switch ($conn.State)
+    {
+        "Open" { $conn.Close() } 
+    }
+
+    $conn.Open()
+}
+
+$masterConnStr = ConnectionString "(local)" "master" "sa" "gavs_123"
+$masterConn  = New-Object System.Data.SqlClient.SQLConnection($masterConnStr)
+
+
+Write-Host "Creating Source DB"
+
+$mastercmd  = new-object System.Data.SQLClient.SQLCommand
+$mastercmd.CommandText = "USE MASTER;
+IF NOT EXISTS ( SELECT  name
+            FROM    sys.databases
+            WHERE   name = N'" + $SrcDatabase + "' )
+CREATE DATABASE " + $SrcDatabase + ";"
+
+$mastercmd.Connection = $masterConn
+ReOpenConnection($masterConn)
+$mastercmd.ExecuteNonQuery()
+Write-Host "Creating Source DB - Completed"
+
+Write-Host "Creating Destination DB"
+$mastercmd.CommandText = "USE MASTER;
+IF NOT EXISTS ( SELECT  name
+            FROM    sys.databases
+            WHERE   name = N'" + $DestDatabase + "' )
+    CREATE DATABASE " + $DestDatabase + ";"
+
+ReOpenConnection($masterConn)
+$mastercmd.Connection = $masterConn
+$mastercmd.ExecuteNonQuery()
+Write-Host "Creating Destination DB - Completed"
+
+Write-Host "Creating Logins"
+$cmdLogin  = new-object System.Data.SQLClient.SQLCommand
+$cmdLogin.CommandText = "USE [master]
+IF  EXISTS (SELECT * FROM sys.server_principals WHERE name = N'" + $SrcUserName + "')
+    DROP LOGIN [" + $SrcUserName + "]
+
+CREATE LOGIN [" + $SrcUserName + "] WITH PASSWORD=N'" + $SrcPwd + "', DEFAULT_DATABASE=[" + $SrcDatabase + "], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+
+USE [" + $SrcDatabase + "]
+CREATE USER [" + $SrcUserName + "] FOR LOGIN [" + $SrcUserName + "]
+ALTER USER [" + $SrcUserName + "] WITH DEFAULT_SCHEMA=[dbo]
+ALTER ROLE [db_owner] ADD MEMBER [" + $SrcUserName + "]
+"
+ReOpenConnection($masterConn)
+$cmdLogin.Connection = $masterConn
+$cmdLogin.ExecuteNonQuery()
+
+$cmdLogin.CommandText = "USE [master]
+IF  EXISTS (SELECT * FROM sys.server_principals WHERE name = N'" + $DestUserName + "')
+    DROP LOGIN [" + $DestUserName + "]
+
+CREATE LOGIN [" + $DestUserName + "] WITH PASSWORD=N'" + $DestPwd + "', DEFAULT_DATABASE=[" + $DestDatabase + "], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+
+USE [" + $DestDatabase + "]
+CREATE USER [" + $DestUserName + "] FOR LOGIN [" + $DestUserName + "]
+ALTER USER [" + $DestUserName + "] WITH DEFAULT_SCHEMA=[dbo]
+ALTER ROLE [db_owner] ADD MEMBER [" + $DestUserName + "]
+"
+ReOpenConnection($masterConn)
+$cmdLogin.Connection = $masterConn
+$cmdLogin.ExecuteNonQuery()
+
+Write-Host "Creating Logins - Completed"
+
+ 
+If ($Truncate) 
+{
+    $TruncateSql = "TRUNCATE TABLE " + $DestTable
+    Sqlcmd -S $DestServer -d $DestDatabase -Q $TruncateSql
+}
+
 $SrcConnStr = ConnectionString $SrcServer $SrcDatabase $SrcUserName $SrcPwd
 $SrcConn  = New-Object System.Data.SqlClient.SQLConnection($SrcConnStr)
 
 Write-Host "Create Source Table"
 $cmd  = new-object System.Data.SQLClient.SQLCommand
-$cmd.CommandText = "CREATE TABLE [dbo].[SourceDB1](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[FirstName] [nvarchar](100) NULL,
-	[LastName] [nvarchar](100) NULL,
-	[DOB] [date] NULL,
-	[Email] [nvarchar](100) NULL,
-	[Department] [nvarchar](100) NULL,
-	[SSIN] [nvarchar](100) NULL,
-	[AccountNumber] [nvarchar](100) NULL,
-	[Address] [nvarchar](400) NULL,
-	[DrivingLicence] [nvarchar](400) NULL
-) ON [PRIMARY]"
-$SrcConn.Open()
+$cmd.CommandText = "SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[" + $SrcTable + "]') AND type in (N'U'))
+BEGIN
+
+    CREATE TABLE [dbo].[" + $SrcTable + "](
+	    [Id] [int] IDENTITY(1,1) NOT NULL,
+	    [FirstName] [nvarchar](100) NULL,
+	    [LastName] [nvarchar](100) NULL,
+	    [DOB] [date] NULL,
+	    [Email] [nvarchar](100) NULL,
+	    [Department] [nvarchar](100) NULL,
+	    [SSIN] [nvarchar](100) NULL,
+	    [AccountNumber] [nvarchar](100) NULL,
+	    [Address] [nvarchar](400) NULL,
+	    [DrivingLicence] [nvarchar](400) NULL,
+        [CreatedDate] [datetime] NOT NULL DEFAULT (getdate()),
+        [ModifiedDate] [datetime] NOT NULL DEFAULT (getdate())
+    ) ON [PRIMARY] 
+END
+
+declare @sqlstmt nvarchar(1000)
+set @sqlstmt ='begin
+declare @iCnt int = 1
+declare @iCntStr varchar(max)
+	while (@iCnt <= 1000)
+		begin
+		set @iCntStr = cast( @iCnt as varchar(9))
+		insert into " + $SrcTable + " ([Id], [FirstName],[LastName],[DOB],[Email],[Department],[SSIN],[AccountNumber],[Address], [DrivingLicence], [CreatedDate] ) values (@@ ,''FirstName'' + @iCntStr, ''LastName'' + @iCntStr , dateadd(MONTH, convert(int,100*rand()), ''1-Jan-1980'') , ''FirstName'' + @iCntStr + ''@gavstech.com'', ''Department'' + @iCntStr , ''100000000'' + @iCnt, ''1250000'' + @iCnt, ''Address, No: '' + @iCntStr , ''Drive000'' + @iCntStr, dateadd(day, -convert(int,10*rand()), getdate()) )
+		set  @iCnt = @iCnt +1
+	end
+end'
+
+exec sp_executesql @sqlstmt"
+ReOpenConnection($SrcConn)
 $cmd.Connection = $SrcConn
 $cmd.ExecuteNonQuery()
-
-#$cmd.CommandText = "declare @sqlstmt nvarchar(1000)
-#set @sqlstmt ='begin
-#declare @iCnt int = 1
-#declare @iCntStr varchar(max)
-#	while (@iCnt <= 1000)
-#		begin
-#		set @iCntStr = cast( @iCnt as varchar(3))
-#		insert into SourceDB1 ([FirstName],[LastName],[DOB],[Email],[Department],[SSIN],[AccountNumber],[Address], [DrivingLicence] ) values (''FirstName'' + @iCntStr, ''LastName'' + @iCntStr , dateadd(MONTH, convert(int,100*rand()), ''1-Jan-1980'') , ''FirstName'' + @iCntStr + ''@gavstech.com'', ''Department'' + @iCntStr , ''100000000'' + @iCnt, ''1250000'' + @iCnt, ''Address, No: '' + @iCntStr , ''Drive000'' + @iCntStr )
-#		set  @iCnt = @iCnt +1
-#		end
-#end'
-#
-#exec sp_executesql @sqlstmt"
-#$cmd.Connection = $SrcConn
-#$cmd.ExecuteNonQuery()
-#$SrcConn.Close()
 
 
 $CmdText = "SELECT * FROM " + $SrcTable
 $SqlCommand = New-Object system.Data.SqlClient.SqlCommand($CmdText, $SrcConn) 
-$SrcConn.Open()
+ReOpenConnection($SrcConn)
 [System.Data.SqlClient.SqlDataReader] $SqlReader = $SqlCommand.ExecuteReader()
 Write-Host "SRC connected"
 Try
@@ -110,18 +190,27 @@ Try
     Write-Host "DEST connected"
 
     write-host "Create Target Table"
-$cmd1  = new-object System.Data.SQLClient.SQLCommand
-$cmd1.CommandText = "CREATE TABLE [dbo].[TargetDB](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[FirstName] [nvarchar](100) NULL,
-	[LastName] [nvarchar](100) NULL,
-	[Email] [nvarchar](100) NULL,
-	[Department] [nvarchar](100) NULL
-) ON [PRIMARY]"
-$tgtConn  = New-Object System.Data.SqlClient.SQLConnection($DestConnStr)
-$tgtConn.Open()
-$cmd1.Connection = $tgtConn
-$cmd1.ExecuteNonQuery()
+    $cmd1  = new-object System.Data.SQLClient.SQLCommand
+    $cmd1.CommandText = "SET ANSI_NULLS ON
+    SET QUOTED_IDENTIFIER ON
+
+    IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[" + $DestTable + "]') AND type in (N'U'))
+    BEGIN
+        CREATE TABLE [dbo].[" + $DestTable + "](
+	        [Id] [int] IDENTITY(1,1) NOT NULL,
+	        [FirstName] [nvarchar](100) NULL,
+	        [LastName] [nvarchar](100) NULL,
+	        [Email] [nvarchar](100) NULL,
+	        [Department] [nvarchar](100) NULL,
+            [CreatedDate] [datetime] NOT NULL DEFAULT (getdate()),
+            [ModifiedDate] [datetime] NOT NULL DEFAULT (getdate())
+        ) ON [PRIMARY]
+    END"
+
+    $tgtConn  = New-Object System.Data.SqlClient.SQLConnection($DestConnStr)
+    ReOpenConnection($tgtConn)
+    $cmd1.Connection = $tgtConn
+    $cmd1.ExecuteNonQuery()
 
     $bulkCopy = New-Object Data.SqlClient.SqlBulkCopy($DestConnStr, [System.Data.SqlClient.SqlBulkCopyOptions]::KeepIdentity)
     $bulkCopy.DestinationTableName = $DestTable
@@ -131,6 +220,7 @@ $cmd1.ExecuteNonQuery()
     $bulkcopy.columnmappings.add("LastName", "LastName");
     $bulkcopy.columnmappings.add("Email", "Email");
     $bulkcopy.columnmappings.add("Department", "Department");
+    $bulkcopy.columnmappings.add("CreatedDate", "CreatedDate");
     $bulkcopy.batchsize = 100
     $bulkcopy.bulkcopytimeout = 120
 
@@ -151,4 +241,13 @@ $cmd1.ExecuteNonQuery()
     $SrcConn.Close()
     $SrcConn.Dispose()
     $bulkCopy.Close()
+
+    $tgtConn.Close()
+    $tgtConn.Dispose()
+
+    $cmdLogin.Dispose()
+
+    $mastercmd.Dispose()
+    $masterConn.Close()
+    $masterConn.Dispose()
   }
