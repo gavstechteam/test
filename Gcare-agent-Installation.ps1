aaff9615ab1d0146ee9045e95b5eed01d74d6929
Madhubala-E <31853326+Madhubala-E@users.noreply.github.com>
2018-06-14 14:36:16 +0530

    Gcare-Agent-Installation-Param(14-6-18)

---
+++ Gcare-agent-Installation.ps1
@@ -0,0 +1,24 @@
+
+md "c:\gcare"
+$out_file="c:\GCare\agentinstall.zip"
+$dest_path="c:\GCare\"
+$install_path= "c:\GCare\GCareAgent.msi"
+$msi_filepath= "c:\GCare\gcare-agent\GCareAgent.msi"
+
+#Download GCare Agent 
+Invoke-WebRequest https://github.com/gavstechteam/test/raw/master/GCareAgent.zip -OutFile $out_file
+
+#Extract and copy the files in GCareagent
+Expand-Archive $out_file -DestinationPath  $dest_path
+
+#Start-Process $install_path -ArgumentList '/quiet' -Wait /log $dest_path+"\gcareinstall.log"
+
+Start-Process "c:\GCare\" GCareAgent.msi
+
+#Start the msi installation
+$msifile= "c:\GCare\gcare-agent\GCareAgent.msi"
+    $arguments= ' /qn /l*v .\log.txt' 
+   Start-Process `
+     -file  $msifile `
+     -arg $arguments `
+     -passthru | wait-process
